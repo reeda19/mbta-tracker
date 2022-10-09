406de7f41bdba1b4ba0e03432110f9813efac34c
Alex Reed <alexreed@Alexs-MacBook-Pro.local>
2022-10-09 15:45:57 -0400

    get mission park data

---
+++ brighamCircle.py
@@ -0,0 +1,22 @@
+# importing the requests library
+import requests
+import datetime
+  
+# api-endpoint
+STOP_ID = 'place-mispk'
+get_url = f'https://api-v3.mbta.com/predictions?filter[stop]={STOP_ID}&sort=arrival_time'
+direction_destinations = ["Heath Street","Union Square"]
+  
+# sending get request and saving the response as response object
+r = requests.get(url = get_url)
+
+# extracting data in json format
+data = r.json()['data']
+#print(data)
+  
+arrival_times = [(train['attributes']['arrival_time'], direction_destinations[train['attributes']['direction_id']]) for train in data[:3]]
+#print(arrival_times)
+for time, dest in arrival_times:
+    minutes_until_arrival = (datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S-04:00') - datetime.datetime.now()).seconds // 60
+    print(f'Train to {dest} arriving at Mission Park in {minutes_until_arrival} minutes')
+
