f3f5aeefaa56caa87af350fcd36b6f3540080253
Alex Reed <alexreed@Alexs-MacBook-Pro.local>
2022-10-27 12:40:41 -0400

    add full stack template

--- brighamCircle.py
+++ brighamCircle.py
@@ -1,7 +1,11 @@
 # importing the requests library
 import requests
 import datetime
-  
+
+# mbta inefficiency dashboard: 
+# Tracks average speed of trains, amount of stoppages/break downs per day per line,
+# most and least efficient lines, avg time between trains, etc
+
 # api-endpoint
 STOP_ID = 'place-mispk'
 get_url = f'https://api-v3.mbta.com/predictions?filter[stop]={STOP_ID}&sort=arrival_time'
@@ -19,4 +23,8 @@ arrival_times = [(train['attributes']['arrival_time'], direction_destinations[tr
 for time, dest in arrival_times:
     minutes_until_arrival = (datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S-04:00') - datetime.datetime.now()).seconds // 60
     print(f'Train to {dest} arriving at Mission Park in {minutes_until_arrival} minutes')
+
+
+
+
 
