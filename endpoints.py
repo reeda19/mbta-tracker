ef2dc8998b36caeaffaf8aa7a525243c5ed5d595
Alex Reed <alexreed@Alexs-MBP.hsd1.ma.comcast.net>
2022-12-12 21:42:20 -0500

    add flask call

--- endpoints.py
+++ endpoints.py
@@ -1,5 +1,10 @@
 import requests
 from enum import Enum
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
 # mbta live map
 # Gets the locations of all trains on a given line and displays them on a map
 
@@ -9,15 +14,17 @@ base_url = f'https://api-v3.mbta.com/'
 
 lines = ['Red', 'Orange', 'Blue', 'Green-B', 'Green-C', 'Green-D', 'Green-E']
 
-        
 
 # get latitudes and longitudes of all trains on a given line
+@app.route('/api/data')
 def get_train_locations_by_line(line):
     get_url = f'https://api-v3.mbta.com/vehicles?filter[route]={line}&include=trip'
-    r = requests.get(url = get_url)
+    r = requests.get(url=get_url)
     data = r.json()['data']
-    locations = [(train['attributes']['latitude'], train['attributes']['longitude']) for train in data if train['attributes']['latitude'] != None and train['attributes']['longitude'] != None]
-    print(locations)
+    locations = [(train['attributes']['latitude'], train['attributes']['longitude'])
+                 for train in data if train['attributes']['latitude'] != None and train['attributes']['longitude'] != None]
+    location_map = {'train'+str(i): locations[i] for i in range(len(locations))}
+    return jsonify(location_map)
 
 
-
+get_train_locations_by_line('Green-E')
