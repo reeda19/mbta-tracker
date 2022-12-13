cd4e48352049bf5f8bb37c148af65119dc58a931
Alex Reed <alexreed@Alexs-MBP.hsd1.ma.comcast.net>
2022-12-12 22:01:55 -0500

    not working

--- endpoints.py
+++ endpoints.py
@@ -1,8 +1,10 @@
 import requests
 from enum import Enum
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 # mbta live map
@@ -17,14 +19,16 @@ lines = ['Red', 'Orange', 'Blue', 'Green-B', 'Green-C', 'Green-D', 'Green-E']
 
 # get latitudes and longitudes of all trains on a given line
 @app.route('/api/data')
-def get_train_locations_by_line(line):
-    get_url = f'https://api-v3.mbta.com/vehicles?filter[route]={line}&include=trip'
+def get_train_locations_by_line():
+    get_url = f'https://api-v3.mbta.com/vehicles?filter[route]=Green-E&include=trip'
     r = requests.get(url=get_url)
     data = r.json()['data']
     locations = [(train['attributes']['latitude'], train['attributes']['longitude'])
                  for train in data if train['attributes']['latitude'] != None and train['attributes']['longitude'] != None]
-    location_map = {'train'+str(i): locations[i] for i in range(len(locations))}
+    location_map = {'train'+str(i): locations[i]
+                    for i in range(len(locations))}
     return jsonify(location_map)
 
 
-get_train_locations_by_line('Green-E')
+if __name__ == '__main__':
+    app.run()
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,32 @@
-import "../styles/globals.css";
-import type { AppType } from "next/dist/shared/lib/utils";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+function App() {
+  const [data, setData] = useState([]);
 
-export default MyApp;
+  useEffect(() => {
+    async function fetchData() {
+      // Make the HTTP request to the Flask server
+      const response = await axios.get('http://localhost:5000/api/data');
+
+      // Update the state with the response data
+      setData(response.data);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data && (
+        <ul>
+          {Object.values(data).map(value => (
+            <li>{value}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
